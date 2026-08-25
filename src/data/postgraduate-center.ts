d1f161063812426795eadce680defddd588f86e9
tumee0005hub <67898418+tumee0005hub@users.noreply.github.com>
2026-08-26 07:30:26 +0800

    Update postgraduate-center.ts
    файлын линк засав

--- src/data/postgraduate-center.ts
+++ src/data/postgraduate-center.ts
@@ -1258,7 +1258,7 @@ const P = "http://postgraduate.mnums.edu.mn/wp-content/uploads";
 export const trainingPrograms: TrainingProgram[] = [
   {
     name: { mn: "Анатомийн эмгэг судлал", en: "Anatomical Pathology" },
-    href: `${P}/2024/12/1.%D0%90%D0%BD%D0%B0%D1%82%D0%BE%D0%BC%D0%B8%D0%B9%D0%BD-%D1%8D%D0%BC%D0%B3%D1%8D%D0%B3-%D1%81%D1%83%D0%B4%D0%BB%D0%B0%D0%BB.pdf`,
+    href: `https://drive.google.com/file/d/1u84Vu3uaHSkYv8RyNYde-DpJ1aifvs4_/view?usp=drive_link`,
   },
   {
     name: { mn: "Гэмтэл согог судлал", en: "Traumatology" },
